f9654299a71b3faccfa96cf879da13c2eb66c4cf
Salomón <115399253+salomoncjl@users.noreply.github.com>
2023-08-15 03:54:57 -0300

    Mantenimiento (#10)
    * JSON mejor estructurado
    
    * Index renovado
    
    * retornarProductos - carga de procutos - boton suma
    
    * arreglo de estructura y enrutamiento
    
    * estructuracion y rutas arregladas
    
    * obtener productos, url y crear
    
    * fix carrito de productos

--- javascript/carrito.js
+++ javascript/carrito.js
@@ -1,9 +1,6 @@
-import productos from "./productos.json"
+import { productos } from "./index.js"
 
-//? creamos nuestro array
-
-//? creando la funcion agregarAlCarrito
-let agregarAlCarrito = (frutaId) => {
+export let agregarAlCarrito = (frutaId) => {
     if (frutaId > 0){
         const encontrado = productos.find(producto => producto.id === parseInt(frutaId))
         if(typeof encontrado !== 'undefined') carritoFrutas.push(encontrado)
@@ -12,28 +9,18 @@ let agregarAlCarrito = (frutaId) => {
 }
 
 const almacenarCarrito = () => {
-    if( carritoFrutas.length > 0 ){
+    if( carritoFrutas.length > 0){
         let carritoConvertido = JSON.stringify(carritoFrutas)
-        localStorage.setItem("carritoFrutas",carritoConvertido)
+        localStorage.setItem("carritoFrutas", carritoConvertido)
     }
 }
 
-const recuperarCarrito = () => {
-    // || => el OR, va a enviar el dato que encuentre como TRUE: estos datos van a lña izquierda y derecha del operador. Ej: datos1 || datos2
+export const recuperarCarrito = () => {
     return JSON.parse(localStorage.getItem("carritoFrutas")) || []
 }
 
 const carritoFrutas = recuperarCarrito()
 
-/*
- AND &&: si la primer comparacion da TRUE, se ejecuta el segundo bloque que le continua al AND 
-
-valida si es true o false && codigo que se va a ejecuta.
-
-const almacenarCarrito = () => carritoFrutas.length > 0 && localStorage.setItem("carritoFrutas",JSON.stringify(carritoFrutas))
-*/
-
 console.table(carritoFrutas);
 
-
-export default agregarAlCarrito
+export default {agregarAlCarrito, recuperarCarrito}
